b42c2695839b213d97504ecee0a410d077c45dc2
Matheus <matheusfarias519@gmail.com>
2023-05-24 17:56:41 -0300

    Created the Path Graph and added regression tests.

--- regress/sql/graph_generation.sql
+++ regress/sql/graph_generation.sql
@@ -45,3 +45,35 @@ SELECT * FROM create_complete_graph(NULL,NULL,NULL);
 SELECT drop_graph('gp1', true);
 SELECT drop_graph('gp2', true);
 
+
+-- PATH GRAPH TESTS
+
+-- Should create 5 vertices with 4 edges. All standard ag_labels.
+SELECT age_create_path('path_graph_5_false', 5, 'PathVertex', 'PATH_EDGE', false);
+SELECT * FROM path_graph_5_false."PathVertex";
+SELECT * FROM path_graph_5_false."PATH_EDGE";
+
+-- Should create 5 vertices with 8 edges. All standard ag_labels.
+SELECT age_create_path('path_graph_5_true', 5, 'PathVertex', 'PATH_EDGE', true);
+SELECT * FROM path_graph_5_true."PathVertex";
+SELECT * FROM path_graph_5_true."PATH_EDGE";
+
+-- Should create 10 vertices with 9 edges. New ag_labels.
+SELECT age_create_path('path_graph_10_false', 10, 'PathVertex', 'PATH_EDGE', false);
+SELECT * FROM path_graph_10_false."PathVertex";
+SELECT * FROM path_graph_10_false."PATH_EDGE";
+
+-- Should create 10 vertices with 18 edges. New ag_labels.
+SELECT age_create_path('path_graph_10_true', 10, 'PathVertex', 'PATH_EDGE', true);
+SELECT * FROM path_graph_10_true."PathVertex";
+SELECT * FROM path_graph_10_true."PATH_EDGE";
+
+-- Should throw errors.
+SELECT age_create_path('path_graph_1', 1, 'PathVertex', 'PATH_EDGE', false);
+SELECT age_create_path(NULL, NULL, NULL, NULL, NULL);
+
+-- Drop Path Graphs.
+SELECT drop_graph('path_graph_5_false', true);
+SELECT drop_graph('path_graph_5_true', true);
+SELECT drop_graph('path_graph_10_false', true);
+SELECT drop_graph('path_graph_10_true', true);
